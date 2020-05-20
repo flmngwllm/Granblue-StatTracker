453b61f948c522657cbb4e90a9085260f3341995
flmngwllm <flmngwllm232@gmail.com>
2020-05-20 18:27:01 -0400

    Installed react-router.Created routes on App.js and added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
+import Nav from './components/Nav'
+import Home from './components/Home'
+import Signup from './components/Signup'
+import User from './components/User'
 
 function App() {
   return (
+   
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav/>
+    <Switch>
+      {/* <Route exact path="/signup" component={Signup}/>
+      <Route exact path="/characters" component={Characters}/>
+      <Route exact path="/user/:id" component={User}/> */}
+      {/* <Route path="/" component={Home}/> */}
+
+
+          </Switch>
     </div>
+  </Router>      
+      
+    
   );
 }
 
